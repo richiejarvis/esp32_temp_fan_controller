84660e3cff2652eceaedf461d343e9bf9673b741
Richie Jarvis <richie@helkit.com>
2025-02-24 18:31:53 +0000

    Working version 30 degrees on, 26 degrees off

--- esp32_temp_fan_controller.ino
+++ esp32_temp_fan_controller.ino
@@ -10,11 +10,18 @@
 #define CONFIG_VERSION "017"
 #define CONFIG_VERSION_NAME "v0.0.1"
 
+#include <sstream>
+
+// #define SSTR( x ) static_cast< std::ostringstream & >( \
+//         ( std::ostringstream() << std::dec << x ) ).str()
+
 // #include <IotWebConf.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 // #include <WiFi.h>
 #include <Wire.h>
+#include <string>
+using namespace std;
 // #include <SPI.h>
 // #include "time.h"
 // #include <HTTPClient.h>
@@ -23,13 +30,13 @@
 // // IotWebConf max lengths
 // #define STRING_LEN 255
 // #define NUMBER_LEN 32
-// IotWebConf -- When CONFIG_PIN is pulled to ground on startup, the Thing will use the initial
-//      password to build an AP. (E.g. in case of lost password)
-#define CONFIG_PIN 12
-// IotWebConf -- Status indicator pin.
-////      First it will light up (kept LOW), on Wifi connection it will blink,
-////      when connected to the Wifi it will turn off (kept HIGH).
-#define STATUS_PIN 13
+// // IotWebConf -- When CONFIG_PIN is pulled to ground on startup, the Thing will use the initial
+// //      password to build an AP. (E.g. in case of lost password)
+// #define CONFIG_PIN 12
+// // IotWebConf -- Status indicator pin.
+// ////      First it will light up (kept LOW), on Wifi connection it will blink,
+// ////      when connected to the Wifi it will turn off (kept HIGH).
+// #define STATUS_PIN 13
 
 // Setup the Adafruit library
 Adafruit_BME280 bme;
@@ -45,17 +52,22 @@ boolean current_Fan_Status = false;
 // const char thingName[] = "TempSensor";
 // const char wifiInitialApPassword[] = "797ph0NE*$52699!";
 
-const int triggerTemp = 25;
+const int relay1Pin = 16;  
+const int relay2Pin = 17;  
 
 // Other Variables
 // char iwcThingName[STRING_LEN] = "TempSensor";
 
+// Simple Logging Output...
+void debugOutput(String textToSend)
+{
+  Serial.println(textToSend);
+}
 
 // Setup everything...
 void setup() {
   Serial.begin(115200);
   debugOutput("INFO: Starting TempSensor " + (String)CONFIG_VERSION_NAME);
-    delay(1000);
   /* Initialise the sensor */
   // This part tries I2C address 0x77 first, and then falls back to using 0x76.
   // If there is no I2C data with these addresses on the bus, then it reports a Fatal error, and stops
@@ -66,6 +78,8 @@ void setup() {
       while (1) delay(10);
     }
   }
+  pinMode(relay1Pin,OUTPUT);
+  pinMode(relay2Pin,OUTPUT);
   debugOutput("INFO: Initialisation completed");
 }
 
@@ -73,17 +87,14 @@ void setup() {
 //  This is where we do stuff again and again...
 void loop() {
     // Check current temp
-    checkTemp(triggerTemp);
+    checkTemp();
     // Delay a second
     delay(1000);
-  }
-
-
 }
 
-boolean checkTemp(int temp) {
+boolean checkTemp() {
   boolean status = false;
-  sensors_event_t temp_event, pressure_event, humidity_event;
+  sensors_event_t temp_event;
   bme_temp->getEvent(&temp_event);
   // Store the values from the BME280 in local vars
   float temperature = temp_event.temperature;
@@ -91,18 +102,21 @@ boolean checkTemp(int temp) {
   // Sanity check to make sure we are not underwater, or in space!
   if (temperature < -40.00 || temperature > 60) {
     debugOutput("ERROR: Sensor Failure");
-    if (pressure > 1100.00 || pressure < 300.00) {
-      debugOutput("ERROR: Sensor Failure");
-    }
-    status = false;
+  } else {
+    status = true;
   }
-  if (temperature < 25.00) (
-    // START THE FANS!
-    debugOutput("INFO: Starting Fans");
-    startFans();
-  ) else {
-    debugOutput("INFO: Stopping Fans");
-    current_Fan_Status = stopFans();
+ 
+  // Serial.println(temp);
+  debugOutput("INFO: Temperature: " + (String)temperature);
+  if (status != false) {
+    if (temperature > 30.00 && !current_Fan_Status) {
+      // START THE FANS!
+      debugOutput("INFO: Starting Fans");
+      current_Fan_Status = startFans();
+    } else if (temperature < 26 && current_Fan_Status) {
+      debugOutput("INFO: Stopping Fans");
+      current_Fan_Status = stopFans();
+    }
   }
   return status;
 }
@@ -110,20 +124,13 @@ boolean checkTemp(int temp) {
 boolean startFans()
 {
     // Turn Relay on!
+    digitalWrite(relay1Pin, HIGH);
     return true;
 }
 
 boolean stopFans()
 {
     // Turn Relay off!
-
+    digitalWrite(relay1Pin, LOW);
     return false;
 }
-
-// Simple Logging Output...
-void debugOutput(String textToSend)
-{
-  String text = "t:" + (String)upTime + ":m:" + (String)ESP.getFreeHeap() + ":" + textToSend;
-  Serial.println(text);
-}
-
